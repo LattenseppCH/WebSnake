3288e04e10170164d278a3036228e5673ec35e9e
LattenseppCH <gianluca@lattensepp.ch>
2025-05-23 09:55:16 +0200

    Fixed Bug: Favicon not showing

--- WebSnakeFlask/app.py
+++ WebSnakeFlask/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_folder='static')
 def index():
     return send_from_directory('static', 'index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(app.static_folder, 'favicon.ico', mimetype='image/x-icon')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
