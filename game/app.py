cd1a7d3d9b3594a85026a90ce8924a95122e7493
LattenseppCH <gianluca@lattensepp.ch>
2025-05-23 15:18:11 +0200

    Bugfix /game/ not available

--- game/app.py
+++ game/app.py
@@ -2,13 +2,17 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__, static_folder='static')
 
-@app.route('/game/')
+@app.route('/')
 def index():
-    return send_from_directory('static', 'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
 
-@app.route('/game/icons/<path:filename>')
+@app.route('/icons/<path:filename>')
 def serve_icons(filename):
     return send_from_directory('static/icons', filename)
+
+@app.route('/<path:filename>')
+def static_files(filename):
+    return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
